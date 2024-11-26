a806c975bfe7bfd9e1c0ca4c8951795733a692a0
Adrian Cedeno <adriancedenor1988@gmail.com>
2024-11-25 21:18:13 -0500

    Crear una aplicacion que alerte al usuario de un corte de energia

---
+++ AppAlertasEnergia/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+using System.Diagnostics;
+using AppAlertasEnergia.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppAlertasEnergia.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Sesion()
+        {
+            return View();
+        }
+
+        public IActionResult Mantenimiento()
+        {
+            return View();
+        }
+        public IActionResult Registro()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        public IActionResult Menu()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ AppAlertasEnergia/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AppAlertasEnergia.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
